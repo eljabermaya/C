443a1faeb6fbdd90c8ee85f45b9a60390d2035c4
eljabermaya <157040577+eljabermaya@users.noreply.github.com>
2024-01-18 16:42:48 +0100

    Create PrimoEsercizio.c

---
+++ PrimoEsercizio.c
@@ -0,0 +1,14 @@
+int main (){
+  int numero1;
+  int numero2;
+  printf("inserisci i due numeri\n");
+  scanf("%d %d",&numero1,&numero2);
+  printf("stampami la somma\n%d\n",numero1+numero2);
+  if(numero1+numero2==10){
+      printf("ciao");
+  }else{
+      printf("no");
+  }
+  
+  return 0;
+}
