c046d895bd281816c6babdc4f6a3bba1fc85e9a2
eljabermaya <157040577+eljabermaya@users.noreply.github.com>
2024-01-21 10:37:38 +0100

    Create es facilissimo.c

---
+++ es facilissimo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int numero1;
+    int numero2;
+    printf("inserisci due numeri\n");
+    scanf("%d %d",&numero1,&numero2);
+    printf("esce\n%d\n",numero1+numero2);
+    
+
+
+    return 0;
+}
